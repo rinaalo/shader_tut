1c1b275548f7285798af096c7c455e180d522be5
alor <93737038+rinaalo@users.noreply.github.com>
2024-07-17 22:48:39 +0200

    cool state
    - changing color based on time
    - changing gradient orientation and amount (?) based on time

--- fragment.glsl
+++ fragment.glsl
@@ -3,7 +3,16 @@ precision highp float;
 varying vec2 vTexCoord;
 varying vec4 vVertexColor;
 
+uniform float uTime;
+
 void main() {
+  vec3 colors = vec3(0.5);
   // give shape a color
-  gl_FragColor = vec4(0.5, 0.8, 0.9, 1.0);
+  // colors.x += (sin(uTime * 1. + vTexCoord.x)) / 2.;
+  // colors.y += (sin(uTime * 2. + vTexCoord.x)) / 2.;
+  // colors.z += (sin(uTime * 3. + vTexCoord.x)) / 2.;
+
+  colors += sin(uTime * vec3(1. , 2., 3.) + vTexCoord.x + cos(vTexCoord.y * uTime)) / 2.;
+
+  gl_FragColor = vec4(colors, 1.0);
 }
