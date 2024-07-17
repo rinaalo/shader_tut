055af6d62bf1e89a11b9426fce9d34248ca5e3f6
alor <93737038+rinaalo@users.noreply.github.com>
2024-07-17 23:36:24 +0200

    added video recording yay

--- sketch.js
+++ sketch.js
@@ -1,7 +1,69 @@
 const border = 60;
 let exampleShader;
 
-// load the shader
+let recording = false;
+let recorder;
+let chunks = [];
+
+const frameRate = 60;
+
+function keyPressed() {
+  if (key === 's') {
+    saveGif('capture.gif', 10);
+  }
+
+  if (key === 'r') {
+    if (recording) {
+      // stop recording
+      console.log("Stopped recording");
+      recorder.stop();
+    } else {
+      // start
+      console.log("Started recording");
+      recorder.start();
+    }
+    recording = !recording;
+  }
+
+}
+
+function initRecorder() {
+  chunks.length = 0;
+
+  let stream = document.querySelector('canvas').captureStream(frameRate);
+
+  recorder = new MediaRecorder(stream);
+
+  recorder.ondataavailable = e => {
+    if (e.data.size) {
+      chunks.push(e.data);
+    }
+  };
+
+  recorder.onstop = exportVideo;
+}
+
+function exportVideo(e) {
+  const blob = new Blob(chunks, {type: 'video/webm'});
+
+  // Draw video to screen
+  const videoElement = document.createElement('video');
+  videoElement.setAttribute('id', Date.now());
+  videoElement.controls = true;
+  document.body.appendChild(videoElement);
+  videoElement.src = window.URL.createObjectURL(blob);
+
+  // Download the video
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  document.body.appendChild(a);
+  a.style = 'display: none';
+  a.href = url;
+  a.download = 'recording.webm';
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
+
 function preload() {
   exampleShader = loadShader('vertex.glsl', 'fragment.glsl');
 }
@@ -10,13 +72,10 @@ function setup() {
   createCanvas(400, 400, WEBGL);
   shader(exampleShader);
   noStroke();
+  initRecorder();
   console.log("Press 's' to save as a 10 second gif :3");
-}
+  console.log("Press 'r' to begin and end a video recording >:3");
 
-function keyPressed() {
-  if (key === 's') {
-    saveGif('capture.gif', 10);
-  }
 }
 
 function draw() {
